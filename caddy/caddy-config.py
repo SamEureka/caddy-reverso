f4ec957e2124cba8a083de0fc0af00a0eb6d69a2
SamEureka <samerueka@me.com>
2022-08-31 14:35:23 -0700

    a ton of changes... all working :smile:

--- caddy/caddy-config.py
+++ caddy/caddy-config.py
@@ -1,15 +1,27 @@
+## Caddy Configurator
+## Sam Dennon // 2022
+
 import os
 import re
 import time
 import textwrap
 
+## Removes all the crazy indenting caused by the multi-line strings.
 defunk = textwrap.dedent
 
+## Grab some of the device variables
+DNS_EMAIL = os.environ['DNS_EMAIL'] if 'DNS_EMAIL' in os.environ else ''
+DNS_PROVIDER = os.environ['DNS_PROVIDER'] if 'DNS_PROVIDER' in os.environ else 'XXX'
+DNS_API_KEY = os.environ['DNS_API_KEY'] if 'DNS_API_KEY' in os.environ else 'XXX'
+
+## Pulls all the HOSTs out of the device variables and puts them in a list of dicts.
+## Format for device variable - Name: HOST_<number>, Value: <host>|<domain>|<ip>|<port>|<wildcard (true or false)>
+## The name must start with 'HOST_' and have a number. The value must separated with the pipe symbol '|' 
 def create_env_list():
   output = []
   for key, val in os.environ.items():
-    if re.match("HOST_[0-9]", key):
-      host = val.split('|')      
+    if re.match("HOST_[0-9]", key): ## Device variable name REGEX.
+      host = val.split('|') ## Splits the values on the pipe separator.      
       output.append({
         'host': host[0],
         'domain': host[1],
@@ -17,10 +29,16 @@ def create_env_list():
         'port': host[3],
         'wildcard': host[4]
       }) 
-  return output
+  if (output != []):
+    return output
+  else:
+    return []
 
+## Creates the site block of the Caddyfile
 def generate_site_block():
   list = create_env_list()
+  if (list == []):
+    return '!! No hosts found !! '
   temp_list = []
   for e in list:
     if (e['wildcard'] == 'true'):
@@ -28,16 +46,34 @@ def generate_site_block():
     temp_list.append(f"{e['host']}.{e['domain']} ")
   return ''.join(temp_list)  
 
+## Creates the tls resource options
+## Required device variables - DNS_PROVIDER and DNS_API_KEY
+## Optional device variable - DNS_EMAIL
 def generate_tls_options():
-  tls_temp = f"""
-    tls {{
-      dns {os.environ['DNS_PROVIDER']} {os.environ['DNS_API_KEY']}
-    }}
-  """
-  return tls_temp
-  
+  if (DNS_API_KEY == 'XXX' or DNS_PROVIDER == 'XXX'):
+    return f"""
+          Create DNS_PROVIDER and/or DNS_API_KEY device variables in balena.io console
+    """
+  else:
+    tls_temp = f"""
+      tls {DNS_EMAIL} {{
+        dns {DNS_PROVIDER} {DNS_API_KEY}
+      }}
+    """
+    return tls_temp
+
+## Creates the resource blocks. Will let user know if there are not any hosts to proxy.
 def generate_matcher_options():
   list = create_env_list()
+  if (list == []):
+    return """
+    !! Create HOST_<number> device variable in balena.io console       !!
+    !! Format for device variable -                                    !!
+    !!  Name: HOST_<number>                                            !!
+    !!  Value: <host>|<domain>|<ip>|<port>|<wildcard (true or false)>  !!
+    !! The name must start with 'HOST_' and have a number.             !!
+    !! The value must separated with the pipe symbol '|'               !!
+    """
   temp_list = []
   for e in list:
     if (e['wildcard'] == 'true'):
@@ -72,10 +108,15 @@ def write_caddyfile():
     defunked = defunk(temp_string)
     cf.write(defunked)
 
+## Write the Caddyfile to /etc/caddy/Caddyfile
 write_caddyfile()
+
+## Use caddy format to correct any problems with the formatting.
 os.system('caddy fmt --overwrite /etc/caddy/Caddyfile')
+## Print it to console so we can see what is being proxied.
 os.system('cat /etc/caddy/Caddyfile')
-os.system('caddy stop')
+## Start the caddy server passing in our new config file.
 os.system('caddy start --config /etc/caddy/Caddyfile')
-while True:
-    time.sleep(60000)
+## Idle... I couldn't find a better way to keep the service running.
+## Please put in an issue if you know a better way to do this... or better yet, a PR!
+os.system('balena-idle')
